6635ed5dacc3e9260949293a6586375c659ed8eb
saroj-thakur <newerasaroj.2020@gmail.com>
2025-05-27 08:23:06 +0545

    Adding new javascript

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("This is a script in the local repository.");
+
+const fName = "Saroj";
+const lName = "Thakur";
+
+const addName = () => {
+    return fName + " " + lName;
+};
+
+console.log(addName());
